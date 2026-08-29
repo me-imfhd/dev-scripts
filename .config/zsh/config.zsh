f324e01d45d398af9fb1ca3d27e5fda4f51b30fd
Fhd <114667178+me-imfhd@users.noreply.github.com>
2026-08-29 17:13:59 +0530

    add zsh configuration file with history and NVM settings

---
+++ .config/zsh/config.zsh
@@ -0,0 +1,19 @@
+HISTFILE=~/.zsh_history
+HISTSIZE=10000000000
+SAVEHIST=$HISTSIZE
+setopt INC_APPEND_HISTORY
+export HISTTIMEFORMAT="[%F %T] "
+setopt EXTENDED_HISTORY           # Add Timestamp to history
+setopt HIST_FIND_NO_DUPS          # Handling duplicate commands
+setopt HIST_IGNORE_ALL_DUPS       # Handling duplicate commands
+
+autoload colors && colors
+
+export GTK_FILE_CHOOSER_BACKEND=local
+
+# NVM (Node Version Manager)
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export EDITOR="nvim"
